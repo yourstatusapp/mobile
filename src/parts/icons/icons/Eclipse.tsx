83ebfb89c2c7ab5ddd5e4b0533ac9307c9cafda4
twanluttik <twanluttik@gmail.com>
2022-02-06 02:55:55 -0800

    removed from tracking

--- src/parts/icons/icons/Eclipse.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Path, Svg, G } from 'react-native-svg';
-export const Eclipse = ({color,style,size}: {color?:any;style?:any;size:number}) => {return (<Svg style={style} aria-hidden="true"  data-prefix="fas" data-icon="ellipsis-v" 
-   viewBox="0 0 192 512" >
-  <Path fill={color} d="M96 184c39.8 0 72 32.2 72 72s-32.2 72-72 72-72-32.2-72-72 32.2-72 72-72zM24 80c0 39.8 32.2 72 72 72s72-32.2 72-72S135.8 8 96 8 24 40.2 24 80zm0 352c0 39.8 32.2 72 72 72s72-32.2 72-72-32.2-72-72-72-72 32.2-72 72z" ></Path>
-</Svg>);};
--- src/parts/icons/icons/MapMarker.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Path, Svg, G } from 'react-native-svg';
-export const MapMarker = ({color,style,size}: {color?:any;style?:any;size:number}) => {return (<Svg style={style} aria-hidden="true"  data-prefix="fas" data-icon="map-marker" 
-   viewBox="0 0 384 512" >
-  <Path fill={color} d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z" ></Path>
-</Svg>);};
