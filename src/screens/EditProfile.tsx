87a9a7c477daf0a1a9ba4a58c78fe31fb994765a
twanluttik <twanluttik@gmail.com>
2022-04-24 01:42:00 -0700

    fewer changes

--- src/screens/EditProfile.tsx
+++ src/screens/EditProfile.tsx
@@ -1,10 +1,11 @@
 import core, { AppAlert, request } from '@core';
-import { Block, Button, Fill, ModalHeader, Spacer, Text } from '@parts';
+import { Block, Button, Fill, IconButton, ModalHeader, Spacer, Text, TextButton } from '@parts';
 import { usePulse } from '@pulsejs/react';
 import { useNavigation } from '@react-navigation/native';
 import React, { useCallback, useEffect, useState } from 'react';
 import { ActivityIndicator, KeyboardAvoidingView } from 'react-native';
 import styled, { useTheme } from 'styled-components/native';
+import { EditUsername } from './parts/EditUsername';
 
 let timeout: NodeJS.Timeout;
 
@@ -13,93 +14,151 @@ export const EditProfile = () => {
 	const theme = useTheme();
 	const profile = usePulse(core.profile.profile);
 
-	const [HasChanged, SetHasChanged] = useState(false);
+	const [objectmodified, setObjectmodified] = useState({ username: false, location: false, bio: false });
 
-	const [Location, SetLocation] = useState('');
-	const [Bio, SetBio] = useState('');
+	// USERNAME LOGIC
+	const [username, setUsername] = useState('');
+	const [usernameLoading, setUsernameLoading] = useState(false);
+	const [usernameValid, setUsernameValid] = useState(false);
+	const [usernameErrMsg, setUsernameErrMsg] = useState('');
 
-	const [Username, SetUsername] = useState('');
-	const [Available, SetAvailable] = useState(false);
-	const [Loaded, SetLoaded] = useState(false);
-	const [ErrorMessage, SetErrorMessage] = useState('');
-	const [UsernameLoading, SetUsernameLoading] = useState(false);
-	const [UsernameValid, SetUsernameValid] = useState(false);
-	const [UsernameErrMsg, SetUsernameErrMsg] = useState('');
+	const CheckUsernameAvailibty = React.useCallback(
+		(usernameInput: string) => {
+			// check if the object has been modified
+			if (objectmodified.username === false) return;
+
+			setUsernameValid(false);
+			setUsernameErrMsg('');
+			setUsernameLoading(true);
+
+			if (timeout) clearTimeout(timeout);
+
+			timeout = setTimeout(async () => {
+				// check for the same username
+				if (username.toLowerCase() === profile.username.toLowerCase()) {
+					setUsernameErrMsg('');
+
+					setObjectmodified(prevState => {
+						let a = Object.assign({}, prevState);
+						a.username = false;
+						return a;
+					});
+					return;
+				}
+
+				const res = await request<{ valid: boolean }>('post', '/profile/username/check', { data: { username: usernameInput } });
+				setUsernameLoading(false);
+				if (res.data) {
+					setUsernameValid(res.data?.valid);
+					if (!res.data?.valid) setUsernameErrMsg(res.message);
+				}
+			}, 1000);
+		},
+		[objectmodified.username, profile.username, username],
+	);
+
+	useEffect(() => {
+		CheckUsernameAvailibty(username);
+	}, [username]);
+
+	const [loading, setLoading] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
 
 	const saveInformation = useCallback(async () => {
 		if (!profile) return;
 
-		let newData: { username: string; bio: string; location: string } = {};
-		// check which keys has been changed and will be used to be updated
+		setLoading(true);
+		const res = await request('patch', '/profile', { data: { username: username } });
+		setLoading(false);
 
-		if (UsernameValid) {
-			if (Username !== (profile?.username || '')) {
-				newData.username = Username;
-			}
-		}
-
-		if (Location !== (profile?.location || '')) {
-			newData.location = Location;
-		}
-
-		if (Bio !== (profile?.bio || '')) {
-			newData.bio = Bio;
-		}
-
-		const res = await request('patch', '/profile', { data: newData });
 		if (res.data) {
 			AppAlert(true, 'Successfull', 'Profile has been updated');
+			core.profile.profile.patch({ username: username });
 			nav.goBack();
 		} else {
-			SetErrorMessage(res.message);
+			setErrorMessage(res.message);
 		}
-	}, [Username, nav, profile, Location, Bio, UsernameValid]);
+	}, [nav, profile, username]);
 
-	const usernameCheck = async (usernameInput: string) => {
-		SetUsernameValid(false);
-		SetUsernameErrMsg('');
-		SetUsernameLoading(true);
+	// const [HasChanged, SetHasChanged] = useState(false);
 
-		if (timeout) clearTimeout(timeout);
+	// const [Location, SetLocation] = useState('');
+	// const [Bio, SetBio] = useState('');
 
-		timeout = setTimeout(async () => {
-			const res = await request<{ valid: boolean }>('post', '/profile/username/check', { data: { username: usernameInput } });
-			SetUsernameLoading(false);
-			if (res.data) {
-				SetUsernameValid(res.data?.valid);
-				if (!res.data?.valid) SetUsernameErrMsg(res.message);
-			}
-		}, 1000);
-	};
+	// const [Available, SetAvailable] = useState(false);
+	// const [Loaded, SetLoaded] = useState(false);
+
+	// useEffect(() => {
+	// 	if (!profile) return;
+	// 	SetUsername(profile?.username || '');
+	// 	SetLocation(profile?.location || '');
+	// 	SetBio(profile?.bio || '');
+	// }, [profile]);
+
+	// useEffect(() => {
+	// 	SetHasChanged(false);
+	// 	if ((profile.username || '') !== Username) {
+	// 		SetHasChanged(true);
+	// 	}
+	// 	if ((profile.location || '') !== Location) {
+	// 		SetHasChanged(true);
+	// 	}
+	// 	if ((profile.bio || '') !== Bio) {
+	// 		SetHasChanged(true);
+	// 	}
+	// }, [Username, Location, Bio, Available, profile]);
 
 	useEffect(() => {
-		usernameCheck(Username);
-	}, [Username]);
-
-	useEffect(() => {
-		if (!profile) return;
-		SetUsername(profile?.username || '');
-		SetLocation(profile?.location || '');
-		SetBio(profile?.bio || '');
-	}, [profile]);
-
-	useEffect(() => {
-		SetHasChanged(false);
-		if ((profile.username || '') !== Username) {
-			SetHasChanged(true);
-		}
-		if ((profile.location || '') !== Location) {
-			SetHasChanged(true);
-		}
-		if ((profile.bio || '') !== Bio) {
-			SetHasChanged(true);
-		}
-	}, [Username, Location, Bio, Available, profile]);
+		setUsername(profile.username);
+	}, []);
 
 	return (
-		<Block safe flex={1} color={theme.background}>
+		<Block flex={1} color={theme.background}>
 			<KeyboardAvoidingView style={{ flex: 1 }} behavior="padding" keyboardVerticalOffset={70}>
 				<ModalHeader title="Edit Profile" />
+				<Spacer size={25} />
+
+				{/* USERNAME */}
+
+				<Block flex={0} vCenter>
+					<CustomEditInput
+						placeholder="username"
+						placeholderTextColor={theme.textFade}
+						value={username}
+						onChangeText={v => {
+							setObjectmodified(prevState => {
+								let a = Object.assign({}, prevState);
+								a.username = true;
+								return a;
+							});
+							setUsername(v);
+						}}
+						autoCapitalize="none"
+						autoCorrect={false}
+						autoCompleteType="off"
+						style={{
+							borderBottomColor: usernameLoading
+								? theme.darker1
+								: (profile.username || '') === username
+								? theme.darker1
+								: usernameValid
+								? '#62CB4E'
+								: '#FF6161',
+						}}
+					/>
+					{(profile.username || '') != username && usernameLoading && (
+						<ActivityIndicator color={theme.textFadeLight} style={{ position: 'absolute', right: 20, paddingBottom: 20 }} />
+					)}
+					{!!usernameErrMsg && (
+						<Text color="#FF6161" size={12} marginTop={5} marginLeft={20}>
+							{usernameErrMsg}
+						</Text>
+					)}
+				</Block>
+				<Text>{JSON.stringify(objectmodified)}</Text>
+
+				{/* LOCATION */}
+				{/* <ModalHeader title="Edit Profile" />
 				<Spacer size={25} />
 				<Text size={14} color="#D53F3F" paddingLeft={10} paddingBottom={10}>
 					{ErrorMessage}
@@ -107,7 +166,8 @@ export const EditProfile = () => {
 				<Text size={14} color={theme.text} paddingLeft={10} weight="600">
 					Username
 				</Text>
-				<Block flex={0} vCenter style={{ position: 'relative' }}>
+
+				<Block flex={0} vCenter>
 					<CustomEditInput
 						placeholder="username"
 						placeholderTextColor={theme.textFade}
@@ -131,20 +191,15 @@ export const EditProfile = () => {
 					{(profile.username || '') != Username && UsernameLoading && (
 						<ActivityIndicator color={theme.textFadeLight} style={{ position: 'absolute', right: 20, paddingBottom: 20 }} />
 					)}
-				</Block>
-				{!!UsernameErrMsg && (
-					<Text color="#FF6161" size={12} marginTop={5} marginLeft={20}>
-						{UsernameErrMsg}
-					</Text>
-				)}
+				</Block> */}
 
-				<Spacer size={20} />
+				{/* <Spacer size={20} />
 
 				<Text size={14} color={theme.text} paddingLeft={10} weight="600">
 					Location
-				</Text>
+				</Text> */}
 
-				<CustomEditInput
+				{/* <CustomEditInput
 					placeholder="Location"
 					placeholderTextColor={theme.textFadeLight}
 					value={Location}
@@ -157,10 +212,10 @@ export const EditProfile = () => {
 					style={{
 						borderBottomColor: (profile.location || '') == Location ? theme.darker1 : '#62CB4E',
 					}}
-				/>
+				/> */}
 
-				<Spacer size={20} />
-				<Text size={14} color={theme.textFade} paddingLeft={10} weight="600">
+				{/* <Spacer size={20} /> */}
+				{/* <Text size={14} color={theme.textFade} paddingLeft={10} weight="600">
 					Bio
 				</Text>
 				<CustomEditInput
@@ -176,12 +231,10 @@ export const EditProfile = () => {
 					style={{
 						borderBottomColor: (profile.bio || '') == Bio ? theme.darker1 : '#62CB4E',
 					}}
-				/>
+				/> */}
 
 				<Fill />
-				<Block flex={0} paddingHorizontal={10}>
-					<Button text={'Save'} onPress={() => saveInformation()} disabled={HasChanged === false} style={{ marginHorizontal: 20 }} />
-				</Block>
+				<Button text="Save" disabled={loading} onPress={() => saveInformation()} />
 			</KeyboardAvoidingView>
 		</Block>
 	);
@@ -189,7 +242,6 @@ export const EditProfile = () => {
 
 const CustomEditInput = styled.TextInput.attrs({ autoCapitalize: 'none', autoCorrect: false, autoCompleType: 'off' })`
 	color: white;
-	/* background-color: red; */
 	height: 29px;
 	padding-left: 10px;
 	/* padding: 10px; */
